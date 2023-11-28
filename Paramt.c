7d609d6033c250a724c51097e7e9834181d13463
sarthaki-id04 <149144156+sarthaki-id04@users.noreply.github.com>
2023-11-28 11:35:09 +0530

    Create Paramt.c

---
+++ Paramt.c
@@ -0,0 +1,40 @@
+#include<iostream>
+
+Using namespace std;
+
+Class stu
+
+{ private:
+
+int a,b;
+
+Public:
+
+stu(int a1,int b1)
+
+{
+
+a =a1
+
+b =b1
+
+}
+
+Void display()
+
+{
+
+
+
+
+cout<<"value of a is << a <<^ prime prime and value of b is "<<b;
+}
+};
+Intmain ()
+
+{
+
+stu s(12,13);
+
+Return 0;
+}
